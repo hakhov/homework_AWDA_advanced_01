76b69a27c08f23e8a0c37909e853d9654f87f8a7
Karen Dashyan <dashyankaren@outlook.com>
2022-08-25 19:50:45 +0400

    Fibonachi

---
+++ FIB/Fib.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIB
+{
+    class Fib
+    {
+        public int CalcFib(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else return CalcFib(n - 1) + CalcFib(n - 2);
+        }
+    }
+}
---
+++ FIB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FIB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fib obj = new();
+            Console.Write("how many charachters do You want to see -> ");
+            int n=int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(obj.CalcFib(i)+", ");
+            }
+            
+        }
+    }
+}
