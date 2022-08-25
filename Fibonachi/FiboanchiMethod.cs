e42906d163445eb3cdae786b625fc95520926409
Karen Dashyan <dashyankaren@outlook.com>
2022-08-25 14:39:55 +0400

    Fibonachi Homework

---
+++ Fibonachi/FiboanchiMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi
+{
+   public class FiboanchiMethod
+    {
+        
+        public int CalcFib(int n)
+        {
+           
+           if (n == 1 || n==2)
+            {
+                return 1;
+            }
+           else return CalcFib(n - 1) + CalcFib(n - 2);
+            
+            
+
+        }
+    }
+}
