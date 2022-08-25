a92c91f008b4b67a6d5ad181ba845bcb61b92363
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-08-25 23:54:33 +0400

    +fibonacci

---
+++ Fibonacci_rekursia/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("count=");
+int count=int.Parse(Console.ReadLine());
+int[] arr=new int[count];
+arr[0] = 0;
+arr[1] = 1;
+Console.Write(arr[0]+"  "+arr[1] +"  ");
+int i = 2;
+Fibonacci(count);
+void Fibonacci(int count)
+{
+    if (count > 2)
+    {
+        arr[i] = arr [i - 2] + arr [i - 1];
+        Console.Write($"{arr[i]}  ");
+        ++i;
+        Fibonacci(count - 1);
+    }  
+}
