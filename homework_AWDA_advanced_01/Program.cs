88a6c00fb2ff23f13d2257d20c3b375c5253f9eb
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-08-25 23:48:52 +0400

    vuyh

--- homework_AWDA_advanced_01/Program.cs
+++ homework_AWDA_advanced_01/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
