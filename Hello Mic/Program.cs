c144880ca6a25029d7a4045c2e2b92df454880e6
Dayana Tumanyan <dayana.tumanyan02@mail.ru>
2022-08-28 01:29:37 +0400

    Github

---
+++ Hello Mic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_Mic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World123");
+        }
+    }
+}
