a3c1b2890a89c8b706dff015c3d5bdf88a47770d
Dayana Tumanyan <dayana.tumanyan02@mail.ru>
2022-08-28 14:30:55 +0400

    Enum, Method

---
+++ Homework-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            Console.WriteLine("Press any key");
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            key = Console.ReadKey();
+            Console.WriteLine(" ");
+            Console.WriteLine($"You pressed the key {key.Key}");
+
+        }
+    }
+}
