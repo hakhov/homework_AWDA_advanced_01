2bff4394ae658b5e52db6fd0714824b1df787a8e
Gor Khachatryan <khachatryangor11@gmail.com>
2022-08-28 12:35:02 +0400

    Fibonacci

--- homework_AWDA_advanced_01/Program.cs
+++ homework_AWDA_advanced_01/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ռեկուրսիայի կիրառմմաբ ստանալ Ֆիբոնաչչի շաչքի անդամները։
+
+
+Console.WriteLine(Fibonacci(int.Parse(Console.ReadLine())));
