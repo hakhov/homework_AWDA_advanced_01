c5298e9e580df0129939a80fdd10e756f1525d60
Karen Dashyan <dashyankaren@outlook.com>
2022-08-28 20:12:36 +0400

    Button Detection

---
+++ KeyBoardDetection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KeyBoardDetection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo button;
+            do
+            {
+                button = Console.ReadKey();
+                Console.WriteLine($"->You Pressed {button.Key}");
+
+            } while (button.Key!=ConsoleKey.Escape);
+            
+
+        }
+    }
+}
