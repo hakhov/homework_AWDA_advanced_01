2976c9fc42e3e9d78c0e35350b5b05f31eba99a5
Gor Martirosyan <gor_martirosyan_00@mail.ru>
2022-08-28 14:46:18 +0400

    ButtonEnum

---
+++ ButtonEnum/Program.cs
@@ -0,0 +1,12 @@
+﻿ConsoleKeyInfo key;
+Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
+Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+do
+{
+    key = Console.ReadKey();
+    Console.Write(" --- You pressed ");
+    if ((key.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+    if ((key.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+    if ((key.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
+    Console.WriteLine(key.Key.ToString());
+} while (key.Key != ConsoleKey.Escape);
