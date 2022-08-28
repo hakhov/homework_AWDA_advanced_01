4cd8fa904729f5a5fb5492434d475a95daeb208c
Sir-Khotsanyan <sirkhotsanyan@gmail.com>
2022-08-28 15:07:52 +0400

    Homework_28.08.2022

---
+++ Press key/Program.cs
@@ -0,0 +1,9 @@
+﻿//Էկրանինն տպել այն ստեղնի անունը, որը սեղմվել է
+ConsoleKeyInfo PressedKey;
+while (true)
+{
+    PressedKey = Console.ReadKey();
+    Console.WriteLine();
+    Console.Write($"You pressed key: {PressedKey.Key}");
+}
+
