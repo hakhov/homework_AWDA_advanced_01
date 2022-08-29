accff6c2b5a0c68124483881e2a20af8d8469d5d
MrGrigorPC <grisha.esayan@mail.ru>
2022-08-29 10:51:28 +0400

    program showes what number is corresponding the number you entered

---
+++ homework 27.08.22)/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("please enter any key from keyboard");
+ConsoleKeyInfo button = Console.ReadKey();
+Console.WriteLine();
+Console.WriteLine("the number value corresponding the buttton you entered is");
+Console.Write((int)button.KeyChar);
