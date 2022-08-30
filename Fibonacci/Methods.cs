e075e738e12622751c11d88c2d1919d63234656a
niazyan.224@gmail.com <niazyan.224@gmail.com>
2022-08-30 20:56:42 +0300

    Fibonacci

---
+++ Fibonacci/Methods.cs
@@ -0,0 +1,18 @@
+﻿
+   public partial class Program
+   {
+    
+      static int Fibonacci(int i)
+    {
+        if (i <= 1)
+        {
+            return i;
+        }
+        else
+        {
+            return Fibonacci(i - 2) + Fibonacci(i - 1);
+        }
+    }
+}
+
+
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number ");
+int x = int.Parse(Console.ReadLine());
+int[] arr = new int[x];
+for (int i = 0; i < x; i++)
+{
+    arr[i] = Fibonacci(i);
+}
+for (int i = 0; i < x; i++)
+{
+    Console.Write($"{arr[i]}\t");
+}
