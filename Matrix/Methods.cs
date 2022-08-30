990fa050684bbdc82928bcd0cea9a07a986d886b
niazyan.224@gmail.com <niazyan.224@gmail.com>
2022-08-30 20:51:54 +0300

    Matrix

---
+++ Matrix/Methods.cs
@@ -0,0 +1,128 @@
+﻿
+public partial class Program
+{
+    static int[,] Matrix(int a, int x, int y)
+    {
+        int[,] matrix1 = new int[x, y];
+        if (a == 0)
+        {
+            Random rnd1 = new Random();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    matrix1[i, j] = rnd1.Next(-50, 50);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write($"matrix[{i},{j}]= ");
+                    matrix1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        return matrix1;
+    }
+
+
+    static int GetMax(int[,] matrix1)
+    {
+        int max = matrix1[0, 0];
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                if (matrix1[i, j] > max)
+                {
+                    max = matrix1[i, j];
+                }
+            }
+        }
+        return max;
+    }
+    static int GetMin(int[,] matrix1)
+    {
+        int min = matrix1[0, 0];
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                if (matrix1[i, j] < min)
+                {
+                    min = matrix1[i, j];
+                }
+            }
+        }
+        return min;
+    }
+    static int GetSum(int GetMax, int GetMin)
+    {
+        return GetMax + GetMin;
+    }
+    static int[,] Print(int[,] matrix1)
+    {
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                Console.Write($"{matrix1[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+        return matrix1;
+
+    }
+
+    static bool Prime(int x)
+    {
+        if (x <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(x); i++)
+        {
+
+            if (x % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+
+    static int[,] SortofMatrix(int[,] matrix1)
+    {
+        int temp = 0;
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 1; j < matrix1.GetLength(1); j++)
+            {
+                if (matrix1[i, j - 1] > matrix1[i, j])
+                {
+                    temp = matrix1[i, j-1];
+                    matrix1[i, j-1] = matrix1[i, j];
+                    matrix1[i, j] = temp;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                Console.Write($"{matrix1[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+                return matrix1;
+
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("If you want the matrix to be generated automatically press 0 otherwise 1");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Rows = ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Column = ");
+int y = int.Parse(Console.ReadLine());
+int[,] matrix = Matrix(a, x, y);
+Print(matrix);
+Console.WriteLine($"Max = {GetMax(matrix)}:");
+Console.WriteLine();
+Console.WriteLine($"Min = {GetMin(matrix)}:");
+Console.WriteLine();
+Console.WriteLine($"Sum = Max + Min = {GetSum(GetMax(matrix), GetMin(matrix))}:");
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        if (Prime(matrix[i, j]))
+        {
+            count++;
+        }
+    }
+}
+int[] arr4 = new int[count];
+int k = 0;
+for (int i = 0; i < x; i++)
+{
+    for (int j = 0; j < y; j++)
+    {
+        if (Prime(matrix[i, j]))
+        {
+            arr4[k] = matrix[i, j];
+            k++;
+        }
+    }
+}
+
+
+if (count == 0)
+{
+    Console.WriteLine("Don`t exist");
+}
+else
+{
+    Console.Write("Primes are` ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr4[i]}\t");
+    }
+}
+Console.WriteLine();
+SortofMatrix(matrix);
