b2b1765567954ef085e69ca75ef10e1b0955afd1
Anahit Madoyan <agmadoyan@gmail.com>
2022-08-30 14:31:29 +0400

    BUTTON

---
+++ homework 1/Program.cs
@@ -0,0 +1,10 @@
+﻿//buttons
+Console.WriteLine("press any button");
+ConsoleKeyInfo PressedKey;
+while (true)
+{
+
+    PressedKey = Console.ReadKey();
+    Console.WriteLine($"You pressed key: {PressedKey.Key}");
+
+}
