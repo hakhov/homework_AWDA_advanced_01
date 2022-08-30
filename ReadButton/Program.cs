3fb5a804c1cdbafd6b5e5dd9b9de960c633a6b6e
niazyan.224@gmail.com <niazyan.224@gmail.com>
2022-08-30 20:59:33 +0300

    Read button

---
+++ ReadButton/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Press button");
+ConsoleKeyInfo input;
+ConsoleKey x;
+do
+{
+    input = Console.ReadKey(true);
+    x = input.Key;
+    Console.WriteLine($"You press {x} button");
+}
+while (x != ConsoleKey.Escape);
