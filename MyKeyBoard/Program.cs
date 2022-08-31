808dd99a4d9edb89a223ed4a9163d92c2f323be8
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-08-31 14:21:53 +0400

    + enum

---
+++ MyKeyBoard/Program.cs
@@ -0,0 +1,9 @@
+﻿ConsoleKeyInfo sign;
+Console.Write("Input sign : ");
+sign = Console.ReadKey();
+while (sign.Key != ConsoleKey.Escape)
+{
+    Console.WriteLine("\nSign : "+sign.Key+"\n Code sign : "+(int)sign.Key+"\n\n");
+    Console.Write("Input sign : ");
+    sign = Console.ReadKey();
+}
