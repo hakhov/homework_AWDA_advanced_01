3f502d1761d956f7816aee659aa5751987ff6e69
Maria <maria.petrosyan.2003@outlook.com>
2022-09-01 20:00:31 +0400

    Squer Equales

---
+++ 01 Sep/Program.cs
@@ -0,0 +1,21 @@
+﻿// Squer Equales
+
+Console.WriteLine(GetArr(1, -6, 5));
+
+static string GetArr(double a, double b, double c)
+{
+    double d= b*b-4*a*c;
+    if (d >= 0)
+    {
+        double x1 = (-b - Math.Sqrt(d)) / (2 * a);
+        double x2 = (-b + Math.Sqrt(d)) / (2 * a);
+        double[] arr = new double[] { x1, x2 };
+        string X1= x1.ToString();
+        string X2= x2.ToString();
+        string str = "X1="+X1 + ", "+"X2=" + X2;
+        return str;
+
+    }
+    else
+        return "Error";
+}
---
+++ 24 Aug/Program.cs
@@ -0,0 +1,32 @@
+﻿//Fibonacci
+
+Console.WriteLine(Fibonacci(3));
+
+static int Fibonacci(int fib)
+{
+    if (fib <= 1)
+        return fib;
+    return Fibonacci(fib - 1) + Fibonacci(fib - 2);
+
+}
+
+//Array
+
+Random rnd = new();
+int row = rnd.Next(1,20);
+int col = rnd.Next(1,20);
+Console.WriteLine(Array(col, row, col , row));
+
+static int Array(int n, int m, int i, int j)
+{
+    int[,] matrix = new int[n, m];
+    for (i =0; i <= n; i++)
+    {
+        for (j = 0; j <= m; j++)
+        {
+            Console.WriteLine(matrix[i,j]);
+        }
+    }
+    return matrix[i,j];
+
+}
