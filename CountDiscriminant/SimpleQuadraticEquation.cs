8e0e0a560a75b3aa3ac0e141cd9861e3ae09fd18
Ashot Mkhitaryan <mkhitaryan.ashoth@outlook.com>
2022-09-01 23:47:48 +0400

    FullHomeWork_31_08_22

---
+++ CountDiscriminant/SimpleQuadraticEquation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDiscriminant
+{
+
+    public class FirstClass
+    {
+        public int a;
+        public int b;
+        public int c;
+        public double x;
+        public double y;
+
+        public FirstClass()
+        {
+
+        }
+    }
+
+    public class SimpleQuadraticEquation
+    {
+
+
+        public FirstClass GetSolution(int a,int b, int c)
+        {
+            FirstClass ob = new FirstClass();
+       
+                double discriminant = Math.Pow(b, 2) - 4 * c * a;
+
+                if (discriminant > 0)
+                {
+                    ob.x = -b + Math.Sqrt(discriminant) / (2 * a);
+                    ob.y = -b - Math.Sqrt(discriminant) / (2 * a);
+                }
+                else if (discriminant == 0)
+                {
+                    ob.x = -b / (2 * a);
+                    ob.y = ob.x;
+                }
+                else
+                {
+                    ob.x = double.NaN;
+                    ob.y = double.NaN;
+                }
+
+                return ob;
+            }
+        }
+    }
+
--- Discriminant/Program.cs
+++ Discriminant/Program.cs
@@ -1,10 +1,36 @@
 ﻿using CountDiscriminant;
 
 
-QuadraticEquation ob = new QuadraticEquation();
 
+//First Solution
+
+//QuadraticEquation ob = new QuadraticEquation();
+
+//Console.Write("Insert A : ");
+//int.TryParse(Console.ReadLine(),out int a);
+//Console.WriteLine();
+//Console.Write("Insert B : ");
+//int.TryParse(Console.ReadLine(), out int b);
+//Console.WriteLine();
+//Console.Write("Insert C : ");
+//int.TryParse(Console.ReadLine(), out int c);
+//Console.WriteLine();
+
+//double x = 0;
+//double y = 0;
+
+//ob.GetSolution(a,b,c,ref x,ref y);
+
+//Console.WriteLine($"X : {x}  Y : {y}");
+
+
+
+//Second Solution
+
+
+SimpleQuadraticEquation ob1 = new SimpleQuadraticEquation();
 Console.Write("Insert A : ");
-int.TryParse(Console.ReadLine(),out int a);
+int.TryParse(Console.ReadLine(), out int a);
 Console.WriteLine();
 Console.Write("Insert B : ");
 int.TryParse(Console.ReadLine(), out int b);
@@ -13,9 +39,13 @@ Console.Write("Insert C : ");
 int.TryParse(Console.ReadLine(), out int c);
 Console.WriteLine();
 
-double x = 0;
-double y = 0;
+FirstClass obb = ob1.GetSolution(a, b, c);
 
-ob.GetSolution(a,b,c,ref x,ref y);
+Console.WriteLine($"X : {obb.x}  Y : {obb.y}");
 
-Console.WriteLine($"X : {x}  Y : {y}");
+
+
+
+
+
+
