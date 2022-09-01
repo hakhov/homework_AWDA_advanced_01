df20c187930b1e0ed97adcca7c826456b409d4a6
Ashot Mkhitaryan <mkhitaryan.ashoth@outlook.com>
2022-09-01 23:18:10 +0400

    Homework_31.08_22

---
+++ CountDiscriminant/QuadraticEquation.cs
@@ -0,0 +1,29 @@
+﻿namespace CountDiscriminant
+{
+    public class QuadraticEquation
+    {
+        public (double,double) GetSolution(int a,int b, int c,ref double x, ref double y)
+        {
+            double discriminant = Math.Pow(b, 2) - 4 * c * a;
+            (double, double) sol = (x, y);
+
+            if (discriminant>0)
+            {
+                x = -b + Math.Sqrt(discriminant)/(2*a);
+                y = -b - Math.Sqrt(discriminant)/(2*a);
+            }
+            else if (discriminant == 0)
+            {
+                x = -b/(2*a);
+                y = x;
+            }
+            else
+            {
+                x = double.NaN;
+                y = double.NaN;
+            }
+
+            return (x,y);
+        }
+    }
+}
---
+++ Discriminant/Program.cs
@@ -0,0 +1,21 @@
+﻿using CountDiscriminant;
+
+
+QuadraticEquation ob = new QuadraticEquation();
+
+Console.Write("Insert A : ");
+int.TryParse(Console.ReadLine(),out int a);
+Console.WriteLine();
+Console.Write("Insert B : ");
+int.TryParse(Console.ReadLine(), out int b);
+Console.WriteLine();
+Console.Write("Insert C : ");
+int.TryParse(Console.ReadLine(), out int c);
+Console.WriteLine();
+
+double x = 0;
+double y = 0;
+
+ob.GetSolution(a,b,c,ref x,ref y);
+
+Console.WriteLine($"X : {x}  Y : {y}");
