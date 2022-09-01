4438b17a47e0b79d06abb5e043695045bb3df27f
pandunts9 <npandunts@gmail.com>
2022-09-01 22:22:38 +0400

    Square equation roots solutions with ref and without tuple

--- VarOnTheMethod/Program.cs
+++ VarOnTheMethod/Program.cs
@@ -10,11 +10,25 @@ namespace VarOnTheMethod
     {
         public static void Main()
         {
-            (double,double) result = SquareEquation(2, 4, 1);
+            double firstCoefficient = Convert.ToDouble(Console.ReadLine());
+            double secondCoefficient = Convert.ToDouble(Console.ReadLine());
+            double thirdCoefficient = Convert.ToDouble(Console.ReadLine());
+
+
+            (double,double) result = SquareEquation(firstCoefficient,secondCoefficient,thirdCoefficient);
             Console.WriteLine(result);
 
-        }
+            string result2 = SquareEquatio2(ref firstCoefficient, ref secondCoefficient,ref thirdCoefficient);
+            Console.WriteLine(result2);
 
+        }
+        /// <summary>
+        /// returnes roots of square equation double type
+        /// </summary>
+        /// <param name="A">First coeficcient</param>
+        /// <param name="B">Second coeficcient</param>
+        /// <param name="C">Third coeficcient</param>
+        /// <returns>returnes double numbers</returns>
         static (double, double) SquareEquation(double A, double B, double C)
         {
 
@@ -30,5 +44,34 @@ namespace VarOnTheMethod
             return (root1, root2);
 
         }
+        /// <summary>
+        /// Returnes roots of equation with string value 
+        /// </summary>
+        /// <param name = "A" > First coeficcient</param>
+        /// <param name="B">Second coeficcient</param>
+        /// <param name="C">Third coeficcient</param>
+        /// <returns>Returnes string value</returns>
+        static string SquareEquatio2(ref double A,ref  double B, ref double C)
+        {
+            double root1 = -1;
+            double root2 = -1;
+            //A = 1; B = -5; C = 6; // if this line is active function uses this values for work
+
+            double Diskr = Math.Pow(B, 2) - 4 * A * C;
+            if (Diskr >= 0)
+            {
+                root1 = (-B + Math.Sqrt(Diskr)) / (2 * A);
+                root2 = (-B - Math.Sqrt(Diskr)) / (2 * A);
+
+            }
+
+            if (root1 != root2)
+                return ($"{root1}\t{root2} ");
+            else
+                return ($"{root1}");
+
+        }
+
+
     }
 }
