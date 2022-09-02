12823ae8d0589984acae1961ff5328f550e0c35f
Gor Martirosyan <gor_martirosyan_00@mail.ru>
2022-09-02 22:06:09 +0400

    With tuple

---
+++ RootsOfEquation/Program.cs
@@ -0,0 +1,31 @@
+﻿//With tuple
+Console.Write("a=");
+int a=int.Parse(Console.ReadLine());
+Console.Write("b=");
+int b=int.Parse(Console.ReadLine());
+Console.Write("c=");
+int c=int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetRoots(a,b,c));
+
+static (double,double) GetRoots(int a,int b,int c)
+{
+    (double, double) answer = (0.0, 0.0);
+    int d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        answer.Item1 = (-b + Math.Sqrt(d)) / (2 * a);
+        answer.Item2 = (-b - Math.Sqrt(d)) / (2 * a);
+    }
+    else if (d == 0)
+    {
+        answer.Item1 = -b / (2 * a);
+        answer.Item2 = answer.Item1;
+    }
+    else
+    {
+        throw new Exception();
+    }
+    return answer;
+}
+
