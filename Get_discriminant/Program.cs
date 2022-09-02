aa4f6968ba1b3f4972eacd14b0b6487e8f087dfc
Sir-Khotsanyan <sirkhotsanyan@gmail.com>
2022-09-02 19:27:45 +0400

    Homework_02.09.2022

---
+++ Get_discriminant/Program.cs
@@ -0,0 +1,27 @@
+﻿//Լուծել քառակուսային հավասարումը առանց ref-ի
+Console.Write("a=");
+double a = double.Parse(Console.ReadLine());
+Console.Write("b=");
+double b = double.Parse(Console.ReadLine());
+Console.Write("c=");
+double c = double.Parse(Console.ReadLine());
+Dis(a, b, c);
+static void Dis(double a, double b, double c)
+{
+    double x, x1, x2;
+    double d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        x1 = (-b + Math.Sqrt(d)) / (2 * a);
+        x2 = (-b - Math.Sqrt(d)) / (2 * a);
+        Console.WriteLine($"x1={x1}, x2={x2}");
+    }
+    else
+        if (d == 0)
+    {
+        x = -b / (2 * a);
+        Console.WriteLine($"x={x}");
+    }
+    else
+        Console.WriteLine("Error");
+}
