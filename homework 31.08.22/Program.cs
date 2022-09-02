dd5d5c62e3de140080eae7001310f2f7ba64d217
MrGrigorPC <grisha.esayan@mail.ru>
2022-09-02 23:21:46 +0400

    version of solving problem with ref

--- homework 31.08.22/Program.cs
+++ homework 31.08.22/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.WriteLine("Please neter number a");
+double a =double.Parse(Console.ReadLine());
+Console.WriteLine("Please neter number b");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Please neter number c");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine(GetRoots(ref a,ref b,ref c));
+
+
+
+static (double,double) GetRoots( ref double a,ref double b,ref double c)
+{
+    double x1=-1,x2=-1;
+    double D = b * b - 4 * a * c;
+    if (D >= 0)
+    {
+        x1=(-b+Math.Sqrt(D))/2*a; 
+        x2 = (-b + Math.Sqrt(D))/2*a;
+        return (x1, x2);
+    }
+    return (x1, x2);
+}
