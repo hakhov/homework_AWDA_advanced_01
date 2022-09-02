0ae3b0edaf22f764cc3dee3983d3a8330ed9bd7b
Gor Khachatryan <khachatryangor11@gmail.com>
2022-09-02 20:20:41 +0400

    square equation

--- homework_AWDA_advanced_01/Program.cs
+++ homework_AWDA_advanced_01/Program.cs
@@ -1,4 +1,31 @@
 ﻿//Ռեկուրսիայի կիրառմմաբ ստանալ Ֆիբոնաչչի շաչքի անդամները։
 
 
-Console.WriteLine(Fibonacci(int.Parse(Console.ReadLine())));
+//    static string Fibonacci(int qanak, int i = 1, long a = 0, long b = 1)
+//    {
+//        if (qanak <= 1)
+//            return "nermucvac tivy sxal e";
+
+//        if (i == 1)
+//            return $"1{Fibonacci(qanak, i + 1)}";
+
+//        if (i > qanak)
+//            return "";
+
+//        long tiv1 = a;
+//        long tiv2 = b;
+//        long tiv3 = tiv1 + tiv2;
+//        tiv1 = tiv2;
+//        tiv2 = tiv3;
+//        return $", {tiv3}{Fibonacci(qanak, i + 1, tiv1, tiv2)}";
+
+//    }
+
+
+//Console.WriteLine(Fibonacci(int.Parse(Console.ReadLine())));
+
+
+
+
+
+
---
+++ homework_AWDA_advanced_01/square equation.cs
@@ -0,0 +1,37 @@
+﻿//With Tuple
+
+//static (double, double) se()
+//{
+//    double a = double.Parse(Console.ReadLine());
+//    double b = double.Parse(Console.ReadLine());
+//    double c = double.Parse(Console.ReadLine());
+//    double d = b * b - 4 * a * c;
+//    if (d >= 0)
+//    {
+//        double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+//        double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+//        return (x1, x2);
+//    }
+//    else
+//        return (-1, -1);
+
+//}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//Without Tuple
+
+static string se(double a, double b, double c)
+{
+    double d = b * b - 4 * a * c;
+    if (d >= 0)
+    {
+        double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+        return ($"{x1}, {x2}");
+    }
+    else
+        return ("D<0");
+
+}
+Console.WriteLine(se(-1, -1, -1));
