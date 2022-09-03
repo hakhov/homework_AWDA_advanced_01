b81c6fb2d04dd314413e89f053d033f326db73ed
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-09-03 20:01:40 +0400

    Discriminant with ref

---
+++ DiscriminantWithRef/MyDC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscriminantWithRef
+{
+    public class MyDC
+    {    double Dc;
+        public void Discriminant(Tuple<double,double,double> members, ref double x1,ref double x2)
+        {
+            Dc = members.Item2*members.Item2 - 4 * members.Item1 * members.Item3;
+            if (Dc < 0)
+            { Console.WriteLine("Answer\nHas no root"); }
+            else if(Dc >= 0)
+            {
+                x1 = (-members.Item2 + Math.Sqrt(Dc)) / (2 * members.Item1);
+                x2 = (-members.Item2 - Math.Sqrt(Dc)) / (2 * members.Item1);
+                Console.WriteLine($"Answer\nx1 = {x1}\nx2 = {x2}");
+            }
+        }
+    }
+}
---
+++ DiscriminantWithRef/Program.cs
@@ -0,0 +1,15 @@
+﻿using DiscriminantWithRef;
+
+MyDC obj = new MyDC();
+Console.Write("Formula ( a*x*x+b*x+c = 0 )\nInput value\na = ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("b = ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("c = ");
+double c = double.Parse(Console.ReadLine());
+var members=new Tuple<double,double,double>(a,b,c);
+Console.Write($"Give different values of x for the experiment :\n x1 = ");
+double x1=double.Parse(Console.ReadLine());
+Console.Write("x2 = ");
+double x2 = double.Parse(Console.ReadLine());
+obj.Discriminant(members,ref x1,ref x2);
