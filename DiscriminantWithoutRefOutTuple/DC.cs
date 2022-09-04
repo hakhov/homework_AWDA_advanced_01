b30e573fe0559719e491ec25fcae3c769d01949d
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-09-04 22:42:48 +0400

    +04,09,22

---
+++ DiscriminantWithoutRefOutTuple/DC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscriminantWithoutRefOutTuple
+{
+    public class DC
+    {
+        public string answer;
+        private double dis;
+        public string MyDiscriminant(double a,double b,double c)
+        {
+            dis = b * b - 4 * a * c;
+            if(dis < 0)
+            {
+                return answer;
+            }
+            else 
+            if(dis>=0)
+            {
+               double x1 = (-b + Math.Sqrt(dis)) / (2 * a);
+               double x2 = (-b - Math.Sqrt(dis)) / (2 * a);
+                answer = "Answer\n";
+                answer += x1.ToString() + "\t" + x1.ToString();
+                return answer;
+            }
+        }
+    }
+}
---
+++ DiscriminantWithoutRefOutTuple/Program.cs
@@ -0,0 +1,11 @@
+﻿using DiscriminantWithoutRefOutTuple;
+
+DC obj = new DC();
+obj.answer = "Answer\nHas no root";
+Console.Write("Formula ( a*x*x+b*x+c = 0 )\nInput value\na = ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("b = ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("c = ");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine(obj.MyDiscriminant(a,b,c));
