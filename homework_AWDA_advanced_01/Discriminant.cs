50160f473dde33a3e9cc7992ac51d757d76bfe1e
Hayk-1996 <hayk98837073@gmail.com>
2022-09-05 12:15:29 -0800

    Discriminant with ref and without ref

---
+++ homework_AWDA_advanced_01/Discriminant.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_AWDA_advanced_01
+{
+
+        public class Discriminant
+        {
+
+
+            public void WithRef(int a, int b, int c, ref double x1, ref double x2)
+            {
+
+                if ((Math.Pow(b, 2) - 4 * a * c) >= 0)
+                {
+                    x1 = (-b + (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+                    x2 = (-b - (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+                }
+                else
+                {
+                    Console.WriteLine("Empty sets");
+                    return;
+                }
+
+            }
+
+            public double[] WithoutRef(double a, double b, double c)
+            {
+
+                double[] arr = new double[2];
+                double d = Math.Pow(b, 2) + (-4 * a * c);
+                if (d >= 0)
+                {
+                    arr[0] = (-b + (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+                    arr[1] = (-b - (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
+
+                }
+                else
+                {
+                    Console.WriteLine("Empty sets");
+
+                }
+                return arr;
+
+            }
+
+
+        }
+    }
+
+
+
--- homework_AWDA_advanced_01/Program.cs
+++ homework_AWDA_advanced_01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using homework_AWDA_advanced_01;
+
+Discriminant discriminant = new Discriminant();
+Console.WriteLine("----------with ref-----------");
+double x1 = 0; double x2 = 0;
+discriminant.WithRef(3, 22, 3, ref x1, ref x2);
+Console.WriteLine("x1= " + x1);
+Console.WriteLine("x1= " + x2);
+Console.WriteLine("----------without ref(with array)-----------");
+
+double[] arr = new double[2];
+double a = 3; double b = 22; double c = 3;
+arr = discriminant.WithoutRef(a, b, c);
+Console.WriteLine("x1= " + arr[0] + "\nx2= " + arr[1]);
+
+
+
