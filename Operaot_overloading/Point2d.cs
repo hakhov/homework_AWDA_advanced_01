6d50e74ab9f7acf5b7d70c3fdfe54bf4ad20b440
Ashot Mkhitaryan <mkhitaryan.ashoth@outlook.com>
2022-09-07 20:02:16 +0400

    HomeWork_05_09_22

---
+++ Operaot_overloading/Point2d.cs
@@ -0,0 +1,35 @@
+﻿namespace Operaot_overloading
+{
+    public class Point2d
+    {
+        public int year { get; set; }
+        public int day { get; set; }
+
+        public static Point2d operator - (Point2d point1, Point2d point2)
+        {
+            Point2d point3 = new();
+            point3.day = point1.day - point2.day;
+            point3.year = point1.year - point2.year;
+
+            return point3;
+        }
+
+        public static Point2d operator /(Point2d point2D, Point2d point2D2)
+        {
+            Point2d point2d1 = new();
+            point2d1.year = point2D.year / point2D2.year;
+            point2d1.day = point2D.day / point2D2.day;
+            return point2d1;
+        }
+
+        public static Point2d operator *(Point2d point2D, Point2d point2D2)
+        {
+            Point2d point2d1 = new();
+            point2d1.year = point2D.year * point2D2.year;
+            point2d1.day = point2D.day * point2D2.day;
+            return point2d1;
+        }
+
+
+    }
+}
---
+++ Operator_Overloading/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using Operaot_overloading;
+
+Point2d ob = new();
+ob.year = 2000;
+ob.day = 31;
+
+
+
+Point2d ob1 = new();
+ob1.year = 1500;
+ob1.day = 21;
+
+Point2d ob2 = ob - ob1;
+
+Console.WriteLine(ob2.year + " " + ob2.day);
