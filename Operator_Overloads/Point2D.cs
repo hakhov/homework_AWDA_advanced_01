682b4a836c5996c21bd3ad71cc83e1b1700470ba
Sir-Khotsanyan <sirkhotsanyan@gmail.com>
2022-09-07 19:44:26 +0400

    OperatorOverloading

---
+++ Operator_Overloads/Point2D.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloads
+{
+    public class Point2D
+    {
+        private double _x;
+        private double _y;
+
+        public Point2D(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public Point2D()
+        {
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine(_x + "\t" + _y);
+        }
+
+        public static Point2D operator +(Point2D point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1._x + point2._x;
+            point._y = point1._y + point2._y;
+
+            return point;
+        }
+        public static Point2D operator +(int point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1 + point2._x;
+            point._y = point1 + point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator +(Point2D point1, int point2)
+        {
+            Point2D point = new();
+            point._x = point1._x + point2;
+            point._y = point1._y + point2;
+
+            return point;
+        }
+
+        public static Point2D operator -(Point2D point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1._x - point2._x;
+            point._y = point1._y - point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator -(int point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1 - point2._x;
+            point._y = point1 - point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator -(Point2D point1, int point2)
+        {
+            Point2D point = new();
+            point._x = point1._x - point2;
+            point._y = point1._y - point2;
+
+            return point;
+        }
+
+
+        public static Point2D operator *(Point2D point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1._x * point2._x;
+            point._y = point1._y * point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator *(int point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1 * point2._x;
+            point._y = point1 * point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator *(Point2D point1, int point2)
+        {
+            Point2D point = new();
+            point._x = point1._x * point2;
+            point._y = point1._y * point2;
+
+            return point;
+        }
+
+        public static Point2D operator /(Point2D point1, Point2D point2)
+        {
+            Point2D point = new();
+
+            point._x = point1._x / point2._x;
+            point._y = point1._y / point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator /(int point1, Point2D point2)
+        {
+            Point2D point = new();
+            point._x = point1 / point2._x;
+            point._y = point1 / point2._y;
+
+            return point;
+        }
+
+        public static Point2D operator /(Point2D point1, int point2)
+        {
+            Point2D point = new();
+            point._x = point1._x / point2;
+            point._y = point1._y / point2;
+
+            return point;
+        }
+    }
+}
+
---
+++ Operator_Overloads/Program.cs
@@ -0,0 +1,53 @@
+﻿//Operotor_Overloads
+
+using Operator_Overloads;
+
+Point2D point1 = new(0, 20);
+point1.ShowInfo();
+
+Point2D point2 = new(10, 15);
+point2.ShowInfo();
+Console.WriteLine();
+
+Console.WriteLine("Sum");
+Point2D sum = point1 + point2;
+sum.ShowInfo();
+
+Point2D sum1 = 7 + point2;
+sum1.ShowInfo();
+
+Point2D sum2 = point1 + 8;
+sum2.ShowInfo();
+Console.WriteLine();
+
+Console.WriteLine("Sub");
+Point2D sub = point1 - point2;
+sub.ShowInfo();
+
+Point2D sub1 = 7 - point2;
+sub1.ShowInfo();
+
+Point2D sub2 = point1 - 8;
+sub2.ShowInfo();
+Console.WriteLine();
+
+Console.WriteLine("Mul");
+Point2D mul = point1 * point2;
+mul.ShowInfo();
+
+Point2D mul1 = 7 * point2;
+mul1.ShowInfo();
+
+Point2D mul2 = point1 * 8;
+mul2.ShowInfo();
+Console.WriteLine();
+
+Console.WriteLine("Div");
+Point2D div = point1 / point2;
+div.ShowInfo();
+
+Point2D div1 = 7 / point2;
+div1.ShowInfo();
+
+Point2D div2 = point1 / 8;
+div2.ShowInfo();
