77ed3097171a3c33a8fd24524d1105de8cefc80b
Hayk-1996 <hayk98837073@gmail.com>
2022-09-06 22:52:19 -0800

    Operator For Methods

---
+++ OperatorForMethods/Operation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorForMethods
+{
+    public class Operation
+    {
+        public int a = 900;
+        public int b = 310;
+
+        public static Operation operator +(Operation ob1, Operation ob2)
+        {
+            Operation ob = new Operation();
+            ob.a = ob1.a + ob2.b;
+            return ob;
+        }
+
+        public static Operation operator -(Operation ob1, Operation ob2)
+        {
+            Operation ob = new Operation();
+            ob.a = ob1.a - ob2.b;
+            return ob;
+        }
+
+        public static Operation operator *(Operation ob1, Operation ob2)
+        {
+            Operation ob = new Operation();
+            ob.a = ob1.a * ob2.b;
+            return ob;
+        }
+        public static Operation operator /(Operation ob1, Operation ob2)
+        {
+            Operation ob = new Operation();
+            ob.a = ob1.a / ob2.b;
+            return ob;
+        }
+
+        public static Operation operator %(Operation ob1, Operation ob2)
+        {
+            Operation ob = new Operation();
+            ob.a = ob1.a % ob2.b;
+            return ob;
+        }
+    }
+}
---
+++ OperatorForMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using OperatorForMethods;
+
+Operation ob1 = new Operation();
+Operation ob2 = new Operation();
+
+Operation obS = ob1 - ob2;
+Operation obA = ob1 + ob2;
+Operation obD = ob1 / ob2;
+Operation obM = ob1 * ob2;
+Operation obR = ob1 % ob2;
+
+Console.WriteLine(obA.a);
