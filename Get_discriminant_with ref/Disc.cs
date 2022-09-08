30eb99546f31f13f799c7169bf2c961f14afb7c7
Sir-Khotsanyan <sirkhotsanyan@gmail.com>
2022-09-08 14:29:06 +0400

    Get discriminant with ref

---
+++ Get_discriminant_with ref/Disc.cs
@@ -0,0 +1,20 @@
+﻿namespace Get_discriminant_with_ref
+{
+    public class Disc
+    {
+        public static (double, double) GetX(ref double a, ref double b, ref double c)
+        {
+            double x1 = 0, x2 = 0, x = 0;
+            double D = b * b - 4 * a * c;
+            if (D >= 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            }
+            else
+                throw new Exception("The discriminant is negative");
+            return (x1, x2);
+
+        }
+    }
+}
---
+++ Get_discriminant_with ref/Program.cs
@@ -0,0 +1,10 @@
+﻿using Get_discriminant_with_ref;
+
+Console.Write("a=");
+double a = double.Parse(Console.ReadLine());
+Console.Write("b=");
+double b = double.Parse(Console.ReadLine());
+Console.Write("c=");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine(Disc.GetX(ref a, ref b, ref c));
