0bf20c313ef14338978cc2f4f037778379f85186
Dayana Tumanyan <dayana.tumanyan02@mail.ru>
2022-09-09 23:16:10 +0400

    Increment-decremnet

---
+++ Homework-6/Program.cs
@@ -0,0 +1,27 @@
+﻿using Homework_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point2D point1 = new Point2D(5, 6);
+        Point2D point2 = new Point2D(1, -3);
+
+        //point1.ShowInfo();
+        //(++point1).ShowInfo();
+        //point1.ShowInfo();
+
+        //point2.ShowInfo();
+        //(--point2).ShowInfo();
+        //point2.ShowInfo();
+
+        point1.ShowInfo();
+        point1++.ShowInfo();
+        point1.ShowInfo();
+
+        point1.ShowInfo();
+        point1--.ShowInfo();
+        point1.ShowInfo();
+    }
+}
+
