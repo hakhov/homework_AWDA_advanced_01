16a9bd6f9fc7be83baebc439d9f4de628f1fe09c
Dayana Tumanyan <dayana.tumanyan02@mail.ru>
2022-09-09 23:22:24 +0400

    Increment, decrement

---
+++ Homework6/Operator.cs
@@ -0,0 +1,35 @@
+﻿ namespace Homework6
+{
+    public class Point2D
+    {
+        private int _x;
+        private int _y;
+        public Point2D(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+        public Point2D() { }
+        public void ShowInfo()
+        {
+            Console.WriteLine(_x + "  " + _y);
+        }
+        public static Point2D operator ++(Point2D point2d)
+        {
+            Point2D result = new Point2D();
+            result._x = point2d._x + 1;
+            result._y = point2d._y + 1;
+            return result;
+        }
+        public static Point2D operator --(Point2D point2d)
+        {
+            Point2D result = new Point2D();
+            result._x = point2d._x - 1;
+            result._y = point2d._y - 1;
+            return result;
+        }
+    }
+}
+
+
+
