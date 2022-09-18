8d277f96fc59ab3adb47dfb091aef063e2688ba2
niazyan.224@gmail.com <niazyan.224@gmail.com>
2022-09-18 23:30:20 +0300

    Explisit_Implisit` *,+,-, /

---
+++ Explisit_Implisit/ClassA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explisit_Implisit
+{
+    internal class ClassA
+    {
+        public double a;
+        public double b;
+
+        public ClassA(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public ClassA()
+        {
+
+        }
+
+        public static ClassA operator +(ClassA classA, ClassA classB)
+        {
+            ClassA classA1 = new();
+            classA1.a = classA.a + classB.a;
+            classA1.b = classA.b + classB.b;
+            return classA1;
+        }
+        public static ClassA operator -(ClassA classA, ClassA classB)
+        {
+            ClassA classA1 = new();
+            classA1.a = classA.a - classB.a;
+            classA1.b = classA.b - classB.b;
+            return classA1;
+        }
+        public static ClassA operator *(ClassA classA, ClassA classB)
+        {
+            ClassA classA1 = new();
+            classA1.a = classA.a * classB.a;
+            classA1.b = classA.b * classB.b;
+            return classA1;
+        }
+        public static ClassA operator /(ClassA classA, ClassA classB)
+        {
+            ClassA classA1 = new();
+            classA1.a = classA.a / classB.a;
+            classA1.b = classA.b / classB.b;
+            return classA1;
+        }
+
+    }
+}
---
+++ Explisit_Implisit/Program.cs
@@ -0,0 +1,22 @@
+﻿using Explisit_Implisit;
+
+ClassA classA = new ClassA(20,60);
+ClassA classB = new ClassA(40,80);
+ClassA classA1 = new();
+ClassA classA2 = new();
+ClassA classA3 = new();
+ClassA classA4 = new();
+classA1 = classA + classB;
+classA2 = classA * classB;
+classA3 = classA - classB;
+classA4 = classA / classB;
+
+PrintInfo(classA1);
+PrintInfo(classA2);
+PrintInfo(classA3);
+PrintInfo(classA4);
+
+void PrintInfo(ClassA classA)
+{
+    Console.WriteLine($"{classA.a}\t{classA.b}");
+}
