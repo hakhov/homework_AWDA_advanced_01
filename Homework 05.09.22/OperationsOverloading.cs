080b5b85be6c70cc9bf8d3ec5ad5c2e0c2ad0ccd
MrGrigorPC <grisha.esayan@mail.ru>
2022-09-18 22:07:12 +0400

    overloading some methos of Point2D class

---
+++ Homework 05.09.22/OperationsOverloading.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationsOverloading
+{
+    internal class Point2D
+    {
+        public double x;
+        public double y;
+
+        public Point2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Point2D()
+        {
+
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"x= {x},y={y}");
+        }
+
+        public static Point2D operator +(Point2D point1, Point2D point2)
+        {
+            Point2D result = new Point2D();
+            result.x = point1.x + point2.x;
+            result.y = point1.y + point2.y;
+
+            return result;
+        }
+
+        public static Point2D operator -(Point2D point1, Point2D point2)
+        {
+            Point2D result = new Point2D();
+            result.x = point1.x - point2.x;
+            result.y = point1.y - point2.y;
+
+            return result;
+        }
+
+        public static Point2D operator *(Point2D point1, Point2D point2)
+        {
+            Point2D result = new Point2D();
+            result.x = point1.x * point2.x;
+            result.y = point1.y * point2.y;
+
+            return result;
+        }
+
+        public static Point2D operator /(Point2D point1, Point2D point2)
+        {
+            Point2D result = new Point2D();
+            result.x = point1.x / point2.x;
+            result.y = point1.y / point2.y;
+
+            return result;
+        }
+
+        public static Point2D operator ++(Point2D point1)
+        {
+            point1.x++;
+            point1.y++;
+            return point1;
+        }
+
+        public static Point2D operator --(Point2D point1)
+        {
+            --point1.x;
+            --point1.y;
+            return point1;
+        }
+        //C# ++ ev -- operatorneri menak mi versian kareli a irakanacnel
+        //pre increment u decrement menak karanq miayn anenq,post versian chi linum classneri hamar overload anel
+
+    }
+}
---
+++ Homework 05.09.22/Program.cs
@@ -0,0 +1,26 @@
+﻿using OperationsOverloading;
+Point2D point1=new Point2D();
+point1.x = 20;
+point1.y = 10;
+
+Point2D point2 = new Point2D(24,16);
+
+Point2D point = point1 + point2;
+Console.WriteLine($"x={point.x},y={point.y}");//44,26
+
+point = point1 - point2;
+Console.WriteLine($"x={point.x},y={point.y}");//-4,-6
+
+point = point1 * point2;
+Console.WriteLine($"x={point.x},y={point.y}");//480,160
+
+point = point1 / point2;
+Console.WriteLine($"x={point.x},y={point.y}");//0.83,0.625
+
+
+
+point1++;
+point2--;
+
+
+
