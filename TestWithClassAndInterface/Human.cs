0b4b651aa64f18c5a0914a1072fa750729be25f5
Karen Dashyan <dashyankaren@outlook.com>
2022-09-20 15:29:03 +0400

    Twst with inherited class and interface

---
+++ TestWithClassAndInterface/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWithClassAndInterface
+{
+    class Human : MyEatingMethod, IEat
+    {
+         void IEat.EatingProcess()
+        {
+            Console.WriteLine("Eating from Interface");
+        }
+
+        public override void EatingProcess()
+        {
+            Console.WriteLine("Eating from Inherited Method");
+        }
+
+       
+    }
+}
---
+++ TestWithClassAndInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestWithClassAndInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.EatingProcess();
+            IEat eat = new Human();
+            eat.EatingProcess();
+            MyEatingMethod myEatingMethod = new Human();
+            myEatingMethod.EatingProcess();
+
+        }
+    }
+}
