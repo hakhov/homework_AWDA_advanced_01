72520057b04218b43e162511c0a313275441379d
Karapet-Grigoryan <112084175+Karapet-Grigoryan@users.noreply.github.com>
2022-10-01 11:52:04 +0400

    overloading of ++,--,+,-,/,* operators in class Point2D

---
+++ OperatorsOverloading/Point2D.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloads
+{
+    internal class Point2D
+    {
+        private (double x, double y) point;
+        public Point2D(double x = 0, double y = 0)
+        {
+            point.x = x;
+            point.y = y;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(point);
+        }
+
+        public static Point2D operator +(Point2D point1, Point2D point2)
+        {
+            Point2D pointt = new();
+            pointt.point.x = point1.point.x + point2.point.x;
+            pointt.point.y = point1.point.y + point2.point.y;
+            return pointt;
+
+        }
+
+        public static Point2D operator ++(Point2D point2D)
+        {
+            point2D.point.x += 1;
+            point2D.point.y += 1;
+            return point2D; 
+        }
+
+        public static Point2D operator -(Point2D point1, Point2D point2)
+        {
+            Point2D pointt = new();
+            pointt.point.x = point1.point.x - point2.point.x;
+            pointt.point.y = point1.point.y - point2.point.y;
+            return pointt;
+
+        }
+
+        public static Point2D operator /(Point2D point1, Point2D point2)
+        {
+            Point2D pointt = new();
+            if (point2.point.x != 0 && point2.point.y != 0 )
+            {
+                pointt.point.x = point1.point.x / point2.point.x;
+                pointt.point.y = point1.point.y / point2.point.y;
+                return pointt;
+            }
+            else
+            {
+                pointt.point.x = 0;
+                pointt.point.y = 0;
+                return pointt;
+            }
+
+        }
+
+        public static Point2D operator *(Point2D point1, Point2D point2)
+        {
+            Point2D pointt = new();
+            pointt.point.x = point1.point.x * point2.point.x;
+            pointt.point.y = point1.point.y * point2.point.y;
+            return pointt;
+
+        }
+
+        public static Point2D operator +(Point2D point1,int x)
+        {
+            Point2D pointt = new();
+            pointt.point.x = point1.point.x + x;
+            pointt.point.y = point1.point.y + x;
+            return pointt;
+
+        }
+
+        public static Point2D operator *(Point2D point1, int x)
+        {
+            Point2D pointt = new();
+            pointt.point.x = point1.point.x * x;
+            pointt.point.y = point1.point.y * x;
+            return pointt;
+
+        }
+    }
+}
---
+++ OperatorsOverloading/Program.cs
@@ -0,0 +1,82 @@
+﻿using OperatorOverloads;
+
+Point2D point_1 = new(10, -4);
+Point2D point_2 = new(0, 5);
+Console.Write("point_1 \t");
+point_1.ShowInfo();
+Console.Write("point_2 \t");
+point_2.ShowInfo();
+
+Console.WriteLine("");
+
+Point2D point_3 = new();
+point_3 = point_1 + point_2;
+Console.Write("point_1 + point_2 \t");
+point_3.ShowInfo();
+
+Console.WriteLine("");
+
+Point2D point_9 = new();
+point_9 = point_1++;
+Console.Write("point_1++ \t");
+point_9.ShowInfo();
+
+Console.WriteLine("");
+//Console.Write((point_1++).ShowInfo());
+
+
+Console.WriteLine("");
+
+Point2D point_10 = new();
+point_10 = ++point_2;
+Console.Write("++point_2 \t");
+point_10.ShowInfo();
+
+Console.WriteLine("");
+//Console.Write(++point_2);
+
+
+Console.WriteLine("");
+
+Point2D point_4 = new();
+point_4 = point_1 - point_2;
+Console.Write("point_1 - point_2 \t");
+point_4.ShowInfo();
+
+Console.WriteLine("");
+
+Point2D point_5 = new();
+point_5 = point_1 / point_2;
+Console.Write("point_1 / point_2 \t");
+point_5.ShowInfo();
+
+Console.WriteLine("");
+
+Point2D point_6 = new();
+point_6 = point_1 * point_2;
+Console.Write("point_1 * point_2 \t");
+point_6.ShowInfo();
+
+Console.WriteLine("");
+
+Point2D point_7 = new();
+point_7 = point_1 + 8;
+Console.Write("point_1 + 8 \t");
+point_7.ShowInfo();
+
+Console.WriteLine("");
+
+Point2D point_8 = new();
+point_8 = point_1 * 3;
+Console.Write("point_1 * 3 \t");
+point_8.ShowInfo();
+
+
+
+int z = 8;
+z++;
+Console.WriteLine(z);
+int k = 8;
+++k;
+Console.WriteLine(k);
+
