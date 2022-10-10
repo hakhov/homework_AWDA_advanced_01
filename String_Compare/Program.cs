33508bbe8ed20e9a9c8c272e136eb1031fae397d
Ashot Mkhitaryan <mkhitaryan.ashoth@outlook.com>
2022-10-10 19:56:40 +0400

    10/10/2022_Homework

---
+++ String_Compare/Program.cs
@@ -0,0 +1,11 @@
+﻿using String_Compare;
+
+StringCompare ob = new StringCompare();
+
+Console.Write("Insert first string : ");
+string first = Console.ReadLine();
+Console.WriteLine();
+Console.Write("Insert second string : ");
+string second = Console.ReadLine();
+
+ob.CompareString(first, second);
---
+++ String_Compare/StringCompare.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Compare
+{
+    public class StringCompare
+    {
+        public void CompareString(string first,string second)
+        {
+            if(first.Length == second.Length)
+            {
+                for (int i = 0; i < first.Length; i++)
+                {
+                    if (first[i] > second[i])
+                    {
+                        Console.WriteLine("First string is bigger");
+                        break;
+                    }
+                    else if (first[i]<second[i])
+                    {
+                        Console.WriteLine("Second string is bigger");
+                        break;
+                    }
+                }
+            }
+            else if(first.Length < second.Length)
+            {
+                Console.WriteLine("Second string is bigger");
+            }
+            else
+            {
+                Console.WriteLine("First string is bigger");
+            }
+        }
+    }
+}
