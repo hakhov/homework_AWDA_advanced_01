f39e27b5c165df47befa2408429fe1e173e20a39
Karen Dashyan <dashyankaren@outlook.com>
2022-10-21 21:55:41 +0400

    Using Lyambda Expressions

---
+++ WorkingWithDelegateAndLyambdaExpressions/Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDelegateAndLyambdaExpressions
+{
+    public delegate double MyDelegate(double number1, double number2);
+    //public class Calc
+    //{
+    //    public double DoOperation(double number1, double number2 ) 
+    //    {
+    //        //double result = myDelegate.Invoke(number1, number2);
+    //        //Console.WriteLine(result);
+    //        return number1 + number2;
+    //    }
+    //    public double Add(double number1, double number2) =>
+        
+    //         number2 + number1;
+
+    //    public double Sub(double number1, double number2) =>
+
+    //         number1 - number2;
+    //    public double Mul(double number1, double number2) =>
+
+    //         number1 * number2;
+
+    //    public double Div(double number1, double number2)
+    //    {
+    //        return number2 != 0 ? number1/number2 : 0;
+    //    }
+
+            
+    }
+}
---
+++ WorkingWithDelegateAndLyambdaExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WorkingWithDelegateAndLyambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calc calc = new Calc();
+            //MyDelegate? myDelegate ;
+            //MyDelegate myDelegate1 = new MyDelegate(calc.Add);
+            //MyDelegate myDelegate2 = new MyDelegate(calc.Sub);
+            //MyDelegate myDelegate3 = new MyDelegate(calc.Mul);
+            //MyDelegate myDelegate4 = new MyDelegate(calc.Div);
+            //myDelegate = myDelegate1 + myDelegate2 + myDelegate3 + myDelegate4;
+            //Console.WriteLine(myDelegate.Invoke(10,14));
+            MyDelegate myDelegateAdd = new MyDelegate((double num1,double num2)=> num1+num2);
+            Console.WriteLine(myDelegateAdd(10,11));
+            MyDelegate myDelegateSub = new MyDelegate((double num1, double num2) => num1 - num2);
+            MyDelegate myDelegateMul = new MyDelegate((double num1, double num2) => num1 * num2);
+            MyDelegate myDelegateDIv = new MyDelegate((double num1, double num2) => num2 != 0 ? num1 / num2 : 0);
+        }
+    }
+}
