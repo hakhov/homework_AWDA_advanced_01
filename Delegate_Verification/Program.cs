baeeb8cee4e55f7d8c8cca84b8eee142130647dd
Dayana Tumanyan <dayana.tumanyan02@mail.ru>
2022-10-30 20:05:27 +0400

    Verification

---
+++ Delegate_Verification/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Delegate_Verification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your details for register");
+
+            Verification ver = new Verification();
+
+            Console.Write("Your Name is ");
+            string name = Console.ReadLine();
+            MyDelegate mydelegate = new MyDelegate(ver.MyName);
+            mydelegate.Invoke(name);
+
+            Console.Write("Your Lastname is ");
+            string lastname = Console.ReadLine();
+            MyDelegate mydelegate1 = new MyDelegate(ver.MyLastName);
+            mydelegate1.Invoke(lastname);
+
+            Console.Write("Your Number is (e.g +374(10)100-100) - ");
+            string number = Console.ReadLine();
+            MyDelegate mydelegate2 = new MyDelegate(ver.MyNumber);
+            mydelegate2.Invoke(number);
+
+            Console.Write("Your Email is ");
+            string email = Console.ReadLine();
+            MyDelegate mydelegate3 = new MyDelegate(ver.MyEmail);
+            mydelegate3.Invoke(email);
+        }
+    }
+}
---
+++ Delegate_Verification/Verification.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Delegate_Verification
+{
+    public delegate void MyDelegate(string people);
+    class Verification
+    {
+        public void MyName(string people)
+        {
+            string peoplename = @"[A-Z]{1}[a-z]";
+            if (Regex.IsMatch(people, peoplename) == false)
+            {
+                Console.WriteLine("Your name was entered incorrectly");
+            }
+        }
+        public void MyLastName(string people)
+        {
+            string peoplelastname = @"[A-Z]{1}[a-z]";
+            if (Regex.IsMatch(people, peoplelastname) == false)
+            {
+                Console.WriteLine("Your lastname was entered incorrectly");
+            }
+        }
+        public void MyNumber(string people)
+        {
+            string phonenumber = @"^\+374\([1-9][0-9]\)[0-9]{3}\-[0-9]{3}";
+            Random rnd = new Random();
+            if (Regex.IsMatch(people, phonenumber) == false)
+            {
+                Console.WriteLine("Your number was entered incorrectly");
+            }
+            else
+            { Console.WriteLine("Your phone verification code is " + rnd.Next(1000, 9999)); }
+        }
+        public void MyEmail(string people)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Your email verification code is " + rnd.Next(1000, 9999));
+        }
+    }
+}
