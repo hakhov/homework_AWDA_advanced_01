82dd44b9bf126cdc66e6821f32e303db6e59074f
Ashot Mkhitaryan <mkhitaryan.ashoth@outlook.com>
2022-11-16 19:35:40 +0400

    homework_11_16_22

---
+++ Generics_withdelegate/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_withdelegate
+{
+
+    public delegate void MyDelegate<T>(T a, T b);
+    public class MyClass
+    {
+        public void func<T>(T a, T b)
+        {
+            Console.WriteLine(a + " " + b);
+        }
+    }
+}
---
+++ Generics_withdelegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using Generics_withdelegate;
+
+MyClass ob = new MyClass();
+MyDelegate<int> myDelegate = new MyDelegate<int>(ob.func);
+
+myDelegate.Invoke(10,52);
+
+MyDelegate<double> myDelegate1 = new MyDelegate<double>(ob.func);
+
+myDelegate1.Invoke(10, 25.8);
+
+
+MyDelegate<bool> myDelegate2 = new MyDelegate<bool>(ob.func);
+
+myDelegate2.Invoke(true, true);
