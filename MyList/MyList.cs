0e49035e244a4e5d0035ac1a118e6328f8eabb4f
Karen Dashyan <dashyankaren@outlook.com>
2022-11-19 01:48:42 +0400

    not all

---
+++ MyList/MyList.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MyList
+{
+    public class MyList<T>
+    {
+        private T[] data;
+        private int size = 0;
+        private int capacity ;
+        public MyList(int initialCapacity=10)
+        {
+            if (initialCapacity<1)
+            {
+                capacity = 1;
+            }
+               this.capacity= initialCapacity;
+            data= new T[capacity];
+        }
+
+        public MyList(T[] values)
+        {
+            //for (int i = 0; i < values.Length; i++)
+            //{
+            //    size = values.Length;
+            //    data[i] = values[i];
+            //}
+            data= values;
+        }
+        public int Size { get { return size; }  }
+        public bool IsEmpty { get { return size == 0;} }
+
+        public T GetAtElement(int index)
+        {
+            ThrowIndexOutOfRange(index);
+            return data[index];
+        }
+        public void SetAtElement(int index, T element)
+        {
+            ThrowIndexOutOfRange(index);
+            data[index] = element;
+        }
+        public void Add(T item)
+        {
+            if (size==capacity)
+            {
+                resize();
+               
+            }
+            data[size++] = item;
+        }
+
+        private void resize()
+        {
+            T[] resizedArray = new T[capacity*2];
+            for (int i = 0; i < capacity; i++)
+            {
+                resizedArray[i] = data[i];
+                //size++;
+            }
+            data = resizedArray;
+            capacity = capacity * 2;
+            
+        }
+
+        private void ThrowIndexOutOfRange(int index)
+        {
+            if (index > size-1  || index<0)
+            {
+         
+                throw new ArgumentOutOfRangeException("invalid index");
+               
+
+            }
+        }
+        public void InsertAtElement(int index, T item)
+        {
+            ThrowIndexOutOfRange(index);
+            if (size == capacity) 
+                resize();
+            else if (size <= capacity)
+            {
+                size = capacity;
+            }
+            for (int i = size; i > index; i--)
+            {
+                data[i] = data[i - 1];
+            }
+            data[index] = item;
+            size++;
+
+        }
+        public void DeleteAtElement(int index)
+        {
+            ThrowIndexOutOfRange(index);
+            T[] resizedArray = new T[size-1];
+            for (int i = index; i < size-1; i++)
+            {
+                data[i] = data[i++];
+                
+            }
+            for (int i = 0; i < resizedArray.Length; i++)
+            {
+                resizedArray[i] = data[i];
+            }
+            data = resizedArray;
+            capacity= capacity--;
+            
+           
+        }
+        //public bool Contains(T item)
+        //{
+        //    if (data is not null)
+        //    {
+        //        for (int i = 0; i < data.Length; i++)
+        //        {
+        //            if (data[i]==item)
+        //            {
+        //            }
+        //        }
+        //    }
+        //    return false;
+        //}
+        public void Clear() { }
+
+        //public int CompareTo(MyList<T>? other)
+        //{
+        //    if (other != null)
+        //    {
+        //        for (int i = 0; i < other.data.Length; i++)
+        //        {
+        //            if (other.data[i] == this.data[i])
+        //            {
+        //                return 0;
+        //            }
+        //        }
+
+        //    }
+        //    throw new NotImplementedException();
+        //}
+        //public static MyList<T> operator = (MyList<T>  d, MyList<T> d1) 
+        //{
+        //    if (d.data is not null && d1.data is not null)
+        //    {
+        //        for (int i = 0; i < d.data.Length; i++)
+        //        {
+        //            for (int j = 0; j < d1.data.Length; j++)
+        //            {
+        //                if (d.data[i] == d1.data[j])
+        //                {
+        //                    return 
+        //                }
+        //            }
+                   
+        //        }
+        //    }
+        //} 
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,37 @@
+﻿using MyList;
+
+int[] ints= new int[6] {1,3,4,5,6,7};
+
+MyList<int> obj = new MyList<int>() ;
+obj.Add(1) ;
+obj.Add(2) ;
+obj.Add(3) ;
+obj.Add(4) ;
+obj.Add(5) ;
+obj.Add(6) ;
+
+Console.WriteLine(obj.GetAtElement(0)); 
+Console.WriteLine(obj.GetAtElement(1)); 
+Console.WriteLine(obj.GetAtElement(2)); 
+Console.WriteLine(obj.GetAtElement(3)); 
+Console.WriteLine(obj.GetAtElement(4)); 
+Console.WriteLine(obj.GetAtElement(5)); 
+
+Console.WriteLine("***********************************");
+obj.DeleteAtElement(3);
+Console.WriteLine(obj.GetAtElement(0));
+Console.WriteLine(obj.GetAtElement(1));
+Console.WriteLine(obj.GetAtElement(2));
+Console.WriteLine(obj.GetAtElement(3));
+Console.WriteLine(obj.GetAtElement(4));
+//Console.WriteLine(obj.GetAtElement(5));
+
+
+MyList<int> obj1 = new MyList<int>(ints);
+Console.WriteLine("___________________________________");
+Console.WriteLine(obj1.GetAtElement(0));
+//Console.WriteLine(obj1.GetAtElement(1));
+//Console.WriteLine(obj1.GetAtElement(2));
+//Console.WriteLine(obj1.GetAtElement(3));
+//Console.WriteLine(obj1.GetAtElement(4));
+//Console.WriteLine(obj1.GetAtElement(5));
