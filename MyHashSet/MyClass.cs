ac99de40b361de1755c2479914815afcf9889706
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-11-19 22:05:15 +0400

    +19.11.22 pakagceri xndir

---
+++ MyHashSet/MyClass.cs
@@ -0,0 +1,22 @@
+﻿   internal class MyClass
+{ 
+     char first;
+     char last;
+    bool val = true;
+    public int count;
+    public MyClass(object? first,object? last,int count)
+    {
+       this.first = (char)first;
+        this.last = (char)last;
+        this.count = count;
+        
+    }
+    public  bool MyMethod()
+    {
+        if ((last - first) > 2)
+           return false; 
+        else
+        return true;
+    }
+    }
+
---
+++ MyHashSet/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+MyClass ob;
+var arrList1 = new ArrayList() { '{', '[', '(', ')', ']', '}' };
+var arrList2 = new ArrayList() { '{', '[', '(', ']', '}', ')' };
+bool val = true;
+int count = arrList1.Count - 1;
+foreach (var item in arrList1)
+{
+    ob = new MyClass(item, arrList1[count],count);
+    if (!ob.MyMethod() )
+    { val = false; break; }
+    --count;
+    if (count == (arrList1.Count - 1) / 2)
+        break;
+}
+Console.WriteLine(val);
+val = true;
+count= arrList2.Count - 1;  
+foreach (var item in arrList1)
+{
+    ob = new MyClass(item, arrList2[count], count);
+    if (!ob.MyMethod())
+    { val = false; break; }
+    --count;
+    if (count == (arrList2.Count - 1) / 2)
+        break;
+}
+Console.WriteLine(val);
+
+
