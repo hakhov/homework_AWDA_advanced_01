b94b606c50665249af0eaa760678f3ad83b55e16
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-11-19 22:09:44 +0400

    +16.11.22 Class work(zangvaci tarer@ chen krknvum)

---
+++ NewList/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = new int[] { 1, 2, 3, 4, 1, 2, 3, 4, 5, 5, 3 };
+Array.Sort(arr);
+int i = 0;
+while (i < arr.Length)
+{
+    if (Array.IndexOf(arr, arr[i]) < Array.LastIndexOf(arr, arr[i]))
+        Console.Write(arr[i] + " , ");
+
+    i = Array.LastIndexOf(arr, arr[i]) + 1;
+}
+
+//List<int> list=new List<int>() { 1,2,3,1,5,2,3,9};
+//list.Sort();
+//int i = 0;
