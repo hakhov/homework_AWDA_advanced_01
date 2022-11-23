2c2e9e9ebbe580cc7245523006671c06c42f6e82
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-11-23 19:03:39 +0400

    +23.11.22 Stack collection and brekets

---
+++ StackCollectionBrekets/Program.cs
@@ -0,0 +1,17 @@
+﻿char[] arr = new char[6] { '{', '[', '(', ')', ']','}'};
+
+Stack<char> stack = new Stack<char> { };
+stack.Push(arr[0]);
+stack.Push(arr[1]);
+stack.Push(arr[2]);
+
+bool answer = false;
+for (int i = 3; i < 6; i++)
+{
+
+    if ((arr[i] == ')' && stack.Pop() == '(') || (arr[i] == '}' && stack.Pop() == '{') || (arr[i] == ']' && stack.Pop() == '['))
+        answer = !answer;
+    else
+        break;
+}
+Console.WriteLine(answer);
