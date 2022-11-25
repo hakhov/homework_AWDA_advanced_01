bcb3c5d3a1778c7ed8875bed925f68c42a164ffd
pandunts9 <npandunts@gmail.com>
2022-11-25 15:28:37 +0400

    Correction for best case if complexity time is O(n)

--- 23_11_LessonWork/MyBubbleSort.cs
+++ 23_11_LessonWork/MyBubbleSort.cs
@@ -12,8 +12,10 @@ namespace _23_11_LessonWork
         {
             int temp;
             int count = 0;
+            bool swapped;
             for (int i = 0; i < arr.Length - 2; i++)
             {
+                swapped = false;
                 for (int j = 1; j < arr.Length - 1; j++)
                 {
                     ++count;
@@ -22,7 +24,13 @@ namespace _23_11_LessonWork
                         temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
+                        swapped = true;
                     }
+                    
+                }
+                if (!swapped)
+                {
+                    break;
                 }
             }
             Console.WriteLine($"Count is {count}");
--- 23_11_LessonWork/Program.cs
+++ 23_11_LessonWork/Program.cs
@@ -7,7 +7,7 @@ namespace _23_11_LessonWork
         static void Main(string[] args)
         {
             int[] array = { -95, 256, -1, 0, 8, 215, -65, 1, 2, 2, 2 };
-            int[] BestArray = { -6,-1,0,8,25,365};
+            int[] BestArray = { -6,-1,0,8,25,365,366,367,388,3666,20000};
 
             MyBubbleSort.MySort(array);
             MyBubbleSort.Print(array);
