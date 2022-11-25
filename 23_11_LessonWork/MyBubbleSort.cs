d332610f1cd89c8e87167dfd6241f7d0b27fcc68
pandunts9 <npandunts@gmail.com>
2022-11-25 15:03:59 +0400

    Best case for bubble and corrections

--- 23_11_LessonWork/MyBubbleSort.cs
+++ 23_11_LessonWork/MyBubbleSort.cs
@@ -10,29 +10,27 @@ namespace _23_11_LessonWork
     {
         public static void MySort(int[] arr)
         {
-            int temp ;
+            int temp;
             int count = 0;
             for (int i = 0; i < arr.Length - 2; i++)
             {
                 for (int j = 1; j < arr.Length - 1; j++)
                 {
                     ++count;
-                    if (arr[j] >= arr[j+1])
+                    if (arr[j] >= arr[j + 1])
                     {
                         temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
                     }
                 }
             }
-            Console.WriteLine(count);
+            Console.WriteLine($"Count is {count}");
         }
         public static void Print(int[] array)
         {
-            foreach (var item in array)
-            {
-                Console.Write($"{string.Join(',' , item)}");
-            }
+            Console.Write($"Sorted array - {string.Join(',', array)}");
+            Console.WriteLine();
         }
     }
 }
--- 23_11_LessonWork/Program.cs
+++ 23_11_LessonWork/Program.cs
@@ -7,8 +7,13 @@ namespace _23_11_LessonWork
         static void Main(string[] args)
         {
             int[] array = { -95, 256, -1, 0, 8, 215, -65, 1, 2, 2, 2 };
+            int[] BestArray = { -6,-1,0,8,25,365};
+
             MyBubbleSort.MySort(array);
             MyBubbleSort.Print(array);
+            Console.WriteLine("\n--------------------------");
+            MyBubbleSort.MySort(BestArray);
+            MyBubbleSort.Print(BestArray);
         }
     }
 }
