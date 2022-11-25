ffd6a6977a5b85f217d164b42553dd7801631670
pandunts9 <npandunts@gmail.com>
2022-11-25 14:52:00 +0400

    Bubble Sort alg.

---
+++ 23_11_LessonWork/MyBubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_11_LessonWork
+{
+    public class MyBubbleSort
+    {
+        public static void MySort(int[] arr)
+        {
+            int temp ;
+            int count = 0;
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                for (int j = 1; j < arr.Length - 1; j++)
+                {
+                    ++count;
+                    if (arr[j] >= arr[j+1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+        public static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write($"{string.Join(',' , item)}");
+            }
+        }
+    }
+}
---
+++ 23_11_LessonWork/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _23_11_LessonWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -95, 256, -1, 0, 8, 215, -65, 1, 2, 2, 2 };
+            MyBubbleSort.MySort(array);
+            MyBubbleSort.Print(array);
+        }
+    }
+}
