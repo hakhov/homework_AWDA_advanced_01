e1ff55824782fb7be79e1118ee2c171787b4c3e4
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-12-02 13:14:21 +0400

    +01.12.22 Classes with operator

---
+++ FromClassesOperator/Classes.cs
@@ -0,0 +1,40 @@
+﻿internal class Classes
+{
+    public int Count;
+    public double Area;
+    public Classes(int count,double area)
+    {
+        Count = count;
+        Area = area;    
+    }
+    public Classes()
+    {
+         
+    }
+    public void Answer()
+    {
+        Console.WriteLine($"Student count : {Count}\nClasses area : {Area}");
+    }
+    public static Classes operator +(Classes classA, Classes classB)
+    {
+        Classes classAll = new Classes();
+        classAll.Count = classA.Count + classB.Count;
+        classAll.Area = classA.Area + classB.Area;
+        return classAll;
+    }
+
+    public static bool operator >(Classes classA, Classes classB)
+    {
+        if (classA.Count > classB.Count)
+            return true;
+        else
+            return false;
+    }
+    public static bool operator <(Classes classA, Classes classB)
+    {
+        if (classA.Count < classB.Count)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ FromClassesOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿Classes classA = new(15, 16.2);
+Classes classB = new(22, 20.5);
+Classes classAll = classB + classA;
+classAll.Answer();
+
+if (classA > classB)
+    Console.WriteLine($"{classA > classB}");
+else
+    Console.WriteLine($"{classA > classB}");
+
+if (classA < classB)
+    Console.WriteLine($"{classA < classB}");
+else
+    Console.WriteLine($"{classA < classB}");
