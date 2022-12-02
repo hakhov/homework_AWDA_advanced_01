6baf2bfb96be3cbc4a21d3dffe52c6f999d5c63a
Gayane Tsaturyan <gayane.tsaturyan.2020@gmail.ru>
2022-12-02 13:16:52 +0400

    +02.12.22 Quick sort

---
+++ MyQuickSort/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] arr = { -1, 2, 5, 7, 4, 85, 77, 2, 3,54 };
+int firstIndex = 0;
+int lastIndex=arr.Length - 1;
+int[] arrSorted=QuickSort(arr, firstIndex, lastIndex);
+Console.WriteLine($"Sorted Array : {string.Join(" , ",arrSorted)}");
+Console.WriteLine($"Count : {count} => Big O(2n)");
---
+++ MyQuickSort/QuickProgram.cs
@@ -0,0 +1,42 @@
+﻿
+public partial class Program
+    {
+    public static int count = 0;
+    public static int[] QuickSort(int[] arr,int firstIndex,int lastIndex)
+    {
+
+        if(firstIndex >=lastIndex)
+        return arr;
+
+        int pivot = GetPivot(arr,  firstIndex, lastIndex);
+        GetPivot(arr, firstIndex, pivot);
+        GetPivot(arr,pivot+1,lastIndex);
+
+        return arr;
+    }
+    private static int GetPivot(int[] arr, int firstIndex, int lastIndex)
+    {
+        int pivot = firstIndex - 1;
+        
+        for(int i = firstIndex; i <= lastIndex;i++)
+        {
+            ++count;
+            if(arr[i]<arr[lastIndex])
+            {
+                pivot++;
+                Swap(ref arr[pivot], ref arr[i]);
+            }
+        }
+        pivot++;
+        Swap(ref arr[pivot],ref arr[lastIndex]);
+        return pivot;   
+    }
+
+    private static void Swap(ref int first, ref int last)
+    {
+        int temp = first;
+        first = last;
+        last= temp;
+    }
+}
+
