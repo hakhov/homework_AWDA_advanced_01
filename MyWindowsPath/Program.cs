22c2913a8aeadc65a2a3a0424631591acf103a5f
Karen Dashyan <dashyankaren@outlook.com>
2022-12-14 00:48:56 +0400

    Windows Folder Info

---
+++ MyWindowsPath/Program.cs
@@ -0,0 +1,22 @@
+﻿DriveInfo[] driveInfos=DriveInfo.GetDrives();
+foreach (DriveInfo driveInfo in driveInfos)
+{
+    string dirPath =@driveInfo.Name;
+	if (Directory.Exists(dirPath))
+	{
+			string[] directories = Directory.GetDirectories(dirPath);
+		foreach (var item in directories)
+		{
+			if (item == @"C:\Windows")
+			{
+				Console.WriteLine("YES");
+				Console.WriteLine(Directory.GetParent(item));
+				Console.WriteLine(Directory.GetDirectoryRoot(item)	);
+				Console.WriteLine(Directory.GetCreationTime(item)	);
+				Console.WriteLine(Directory.GetLastAccessTime(item)	);
+				Console.WriteLine(Directory.GetCreationTime(item)	);
+			}
+			
+		}
+	}
+}
