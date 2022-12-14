7e405129e2852c50a39a3a9fdee193338cf73185
Hunanyan Davit <dav040201@gmail.com>
2022-12-14 12:12:20 +0400

    Getting all directories from C\Windows directory

---
+++ File/File/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string path = @"C:\Windows";
+Info_Diractory(path);
+
+static void Info_Diractory(string path)
+{
+    var dirInfo = Directory.GetDirectories(path);
+
+    foreach (var item in dirInfo)
+    {
+        Console.WriteLine(item);
+    }
+}
