810a0de2804b94354f11b4c2f51d535c1b728585
Karen Dashyan <dashyankaren@outlook.com>
2022-12-18 22:38:43 +0400

    StremImages

---
+++ filestramwithimage/Program.cs
@@ -0,0 +1,10 @@
+﻿string path = @"D:\Karen\MI_9 backup\Images\DCIM";
+DirectoryInfo Directory = new DirectoryInfo(path);
+FileInfo[] files = Directory.GetFiles("*.jpg");
+
+Console.WriteLine($"Found {files.Length} jpg photos");
+
+foreach (FileInfo file in files)
+{
+    Console.WriteLine(file.Name);
+};
