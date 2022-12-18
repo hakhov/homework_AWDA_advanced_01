457ad957d550a5677db3d79d8cebec81c5b31b70
Karen Dashyan <dashyankaren@outlook.com>
2022-12-18 18:50:20 +0400

    filestream

---
+++ MyDiretory/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//string path = @"D:\Test";
+//var x = Directory.CreateDirectory(path);
+//if (x.Exists)
+//{
+//    Directory.Delete(path, true);
+//}
+
+
+string fileName = @"D:\Test.txt";
+FileInfo t=new FileInfo(fileName);
+if (t.Exists)
+{
+    Console.WriteLine("Done!");
+}
+else
+{
+    Console.WriteLine("BAD");
+}
+
+string text = "Hello is a Word that means Barev"+Environment.NewLine;
+//File.AppendAllLines(fileName, text.Split(Environment.NewLine.ToCharArray()).ToList<string>()); 
+File.AppendAllTextAsync(fileName, text);
---
+++ MyFileSTREAM/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+string path = @"D\Test.txt";
+
+FileInfo fileInfo = new FileInfo(path); 
+string text=Console.ReadLine();
+FileStream nt = new FileStream(path,FileMode.OpenOrCreate);
+byte[] buffer = Encoding.Default.GetBytes(text);
+nt.Write(buffer, 0, buffer.Length);
+nt.Close();
